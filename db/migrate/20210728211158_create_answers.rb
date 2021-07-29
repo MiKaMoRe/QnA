85398cf1b64a634194b67883b19e67778ca7a535
MiKaMoRe <kim.roman-2002@yandex.ru>
2021-07-29 14:40:05 +0300

    Add null validation to answer and question tables

--- db/migrate/20210728211158_create_answers.rb
+++ db/migrate/20210728211158_create_answers.rb
@@ -1,8 +1,8 @@
 class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
-      t.string :title
-      t.text :body
+      t.string :title, null: false
+      t.text :body, null: false
       t.reference :question, null: false, foreign_key: true
 
       t.timestamps
