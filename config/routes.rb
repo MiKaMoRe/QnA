cdb0d8e31f69dbbfd0cde4b85814a2b127e2da0e
MiKaMoRe <kim.roman-2002@yandex.ru>
2021-08-09 20:05:10 +0300

    Add User sessions and acceptance tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
+  root to: 'questions#index'
+
   resources :questions, only: %i[create show] do
     resources :answers, only: %i[create show]
   end
