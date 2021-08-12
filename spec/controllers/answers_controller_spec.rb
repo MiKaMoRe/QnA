1b2e72bffe483f35d02735b368d625e39c669991
MiKaMoRe <kim.roman-2002@yandex.ru>
2021-08-12 22:08:44 +0000

    Add delete tests to answers controller

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  let(:user) { create(:user) }
+  let(:question) { create(:question, author: user) }
+  
+  before { login(user) }
+
   describe 'POST #create' do
-    let(:user) { create(:user) }
-    let(:question) { create(:question, author: user) }
-
-    before { login(user) }
-
     context 'with valid attributes' do
       it 'saves a new answer in database' do
         expect { post :create, params: { answer: attributes_for(:answer), question_id: question.id, author_id: user.id  } }.to change(Answer, :count).by(1)
@@ -27,6 +27,19 @@ RSpec.describe AnswersController, type: :controller do
         post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question.id }
         expect(response).to render_template 'questions/show'
       end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let!(:answer) { create(:answer, question: question, author: user )}
+
+    it 'deletes the answer' do
+      expect { delete :destroy, params: { id: answer } }.to change(Answer, :count).by(-1)
+    end
+
+    it 'redirects to index' do
+      delete :destroy, params: { id: answer }
+      expect(response).to redirect_to question_path(answer.question)
     end
   end
 end
