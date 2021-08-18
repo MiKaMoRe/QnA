e149ef14b4f133b785a8a3e2477b1bf783b0a847
MiKaMoRe <kim.roman-2002@yandex.ru>
2021-08-18 20:40:09 +0000

    Controllers specs refactored

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -5,53 +5,38 @@ RSpec.describe AnswersController, type: :controller do
   let(:user) { create(:user) }
   let(:question) { create(:question, author: user) }
 
-  describe 'Authenticated user' do
-    before { login(user) }
+  describe 'POST #create' do
+    context 'Authenticated user' do
+      before { login(user) }
 
-    describe 'POST #create' do
       context 'with valid attributes' do
         let(:answer_params) { attributes_for(:answer) }
 
         it 'saves a new answer in database' do
           expect { post_create }.to change(Answer, :count).by(1)
         end
-  
+
         it 'redirect to show view' do
           post_create
           expect(response).to redirect_to assigns(:question)
         end
       end
-  
+
       context 'with invalid attributes' do
         let(:answer_params) { attributes_for(:answer, :invalid) }
 
         it 'does not save the answer' do
           expect { post_create }.to_not change(Answer, :count)
         end
-  
+
         it 're-render new view' do
           post_create
           expect(response).to render_template 'questions/show'
         end
       end
     end
-  
-    describe 'DELETE #destroy' do
-      let!(:answer) { create(:answer, question: question, author: user )}
-  
-      it 'deletes the answer' do
-        expect { delete :destroy, params: { id: answer } }.to change(Answer, :count).by(-1)
-      end
-  
-      it 'redirects to index' do
-        delete :destroy, params: { id: answer }
-        expect(response).to redirect_to question_path(answer.question)
-      end
-    end
-  end
 
-  describe 'Unauthenticated user' do
-    context 'POST #create' do
+    context 'Unauthenticated user' do
       let(:answer_params) { attributes_for(:answer) }
 
       it 'not saves a new answer in database' do
@@ -63,8 +48,25 @@ RSpec.describe AnswersController, type: :controller do
         expect(response).to redirect_to new_user_session_path
       end
     end
-  
-    describe 'DELETE #destroy' do
+  end
+
+  describe 'DELETE #destroy' do
+    context 'Authenticated user' do
+      let!(:answer) { create(:answer, question: question, author: user )}
+
+      before { login(user) }
+
+      it 'deletes the answer' do
+        expect { delete :destroy, params: { id: answer } }.to change(Answer, :count).by(-1)
+      end
+
+      it 'redirects to index' do
+        delete :destroy, params: { id: answer }
+        expect(response).to redirect_to question_path(answer.question)
+      end
+    end
+
+    context 'Unauthenticated user' do
       let!(:answer) { create(:answer, question: question, author: user )}
   
       it 'not deletes the answer' do
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,23 +4,23 @@ RSpec.describe QuestionsController, type: :controller do
   let(:post_create) { post :create, params: { question: question_params } }
   let(:user) { create(:user) }
 
-  describe 'Authenticated user' do
-    before { login(user) }
+  describe 'POST #create' do
+    context 'Authenticated user' do
+      before { login(user) }
 
-    describe 'POST #create' do
       context 'with valid attributes' do
         let(:question_params) { attributes_for(:question) }
 
         it 'saves a new question in the database' do
           expect { post_create }.to change(Question, :count).by(1)
         end
-  
+
         it 'redirect to show view' do
           post_create
           expect(response).to redirect_to assigns(:question)
         end
       end
-  
+
       context 'with invalid attributes' do 
         let(:question_params) { attributes_for(:question, :invalid) }
 
@@ -35,22 +35,7 @@ RSpec.describe QuestionsController, type: :controller do
       end
     end
 
-    describe 'DELETE #destroy' do
-      let!(:question) { create(:question, author: user )}
-  
-      it 'deletes the question' do
-        expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
-      end
-  
-      it 'redirects to index' do
-        delete :destroy, params: { id: question }
-        expect(response).to redirect_to root_path
-      end
-    end
-  end
-  
-  describe 'Unauthenticated user' do
-    describe 'POST #create' do
+    context 'Unauthenticated user' do
       let(:question_params) { attributes_for(:question) }
 
       it 'saves a new question in the database' do
@@ -62,8 +47,25 @@ RSpec.describe QuestionsController, type: :controller do
         expect(response).to redirect_to new_user_session_path
       end
     end
+  end
 
-    describe 'DELETE #destroy' do
+  describe 'DELETE #destroy' do
+    context 'Authenticated user' do
+      let!(:question) { create(:question, author: user )}
+
+      before { login(user) }
+
+      it 'deletes the question' do
+        expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
+      end
+
+      it 'redirects to index' do
+        delete :destroy, params: { id: question }
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'Unauthenticated user' do
       let!(:question) { create(:question, author: user) }
       
       it 'deletes the question' do
