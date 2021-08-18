ca0601daaf375763dadef29b19a5eb1c808c989a
MiKaMoRe <kim.roman-2002@yandex.ru>
2021-08-18 22:08:44 +0000

    Add show feature spec to question

---
+++ spec/features/question/show_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'User can open the question', %q{
+  In order to find answer
+  As an unautheticated user
+  I'd like to be able open the question
+} do
+  let!(:answer) { create(:answer) }
+  
+  scenario 'User open questions list' do
+    visit question_path(answer.question)
+
+    expect(page).to have_content answer.title
+    expect(page).to have_content answer.body
+    expect(page).to have_content answer.question.title
+    expect(page).to have_content answer.question.body
+  end
+end
