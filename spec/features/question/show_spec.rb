fd673ba2aca9b6a840bb5089083e79d448f38bac
MiKaMoRe <kim.roman-2002@yandex.ru>
2021-08-28 06:38:12 +0000

    Move to new file choose best answer tests

--- spec/features/question/show_spec.rb
+++ spec/features/question/show_spec.rb
@@ -7,65 +7,15 @@ feature 'User can open the question', "
   As an unautheticated user
   I'd like to be able open the question
 " do
-  describe 'when authenticated user' do
-    given(:user) { create(:user) }
-    given(:question) { create(:question, author: user) }
-
-    background do
-      sign_in(user)
-    end
-
-    scenario 'choose the best answer of his question', js: true do
-      create(:answer, question: question)
-      visit question_path(question)
-      click_on 'Choose as best'
-
-      expect(page).not_to have_content 'Choose as best'
-      expect(page).to have_content 'Best answer'
-    end
-
-    scenario 'choose other answer of his question', js: true do
-      create(:answer, question: question, best: true)
-      create(:answer, question: question, body: 'second answer')
-      visit question_path(question)
-
-      within '.answer:nth-child(2)' do
-        click_on 'Choose as best'
-      end
-      
-      within '.answer:nth-child(1)' do
-        expect(page).to have_content 'Best answer'
-      end
-
-      within '.answer:nth-child(2)' do
-        expect(page).not_to have_content 'Best answer'
-      end
-    end
-
-    scenario 'choose a best answer of another question' do
-      visit question_path(create(:question))
-
-      expect(page).not_to have_content 'Choose as best'
-    end
-  end
-
-  describe 'when unauthenticated user' do
-    given!(:answer) { create(:answer) }
-
-    scenario 'user open questions list' do
-      visit root_path
-      click_on 'Open answers'
+  given!(:answer) { create(:answer) }
   
-      expect(page).to have_content answer.title
-      expect(page).to have_content answer.body
-      expect(page).to have_content answer.question.title
-      expect(page).to have_content answer.question.body
-    end
+  scenario 'user open questions list' do
+    visit root_path
+    click_on 'Open answers'
 
-    scenario "choose a best answer" do
-      visit question_path(create(:question))
-
-      expect(page).not_to have_content 'Choose as best'
-    end
+    expect(page).to have_content answer.title
+    expect(page).to have_content answer.body
+    expect(page).to have_content answer.question.title
+    expect(page).to have_content answer.question.body
   end
 end
