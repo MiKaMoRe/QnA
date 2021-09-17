0fe11f3a2241e406be0c1cfc3b77bd2965e201a7
MiKaMoRe <kim.roman-2002@yandex.ru>
2021-09-17 19:39:25 +0300

    Add feature: commet model to comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
 
+  after_action :publish_comment, only: [:create]
+
   def create
     @commentable = params[:commentable_type].constantize.find(params[commentable_id])
     @comment = @commentable.comments.build(comment_params)
@@ -23,6 +25,37 @@ class CommentsController < ApplicationController
   end
 
   private
+
+  def publish_comment
+    return if @comment.errors.any?
+    
+    ActionCable.server.broadcast(
+      'comments', 
+      resource_name: @comment.commentable.class.name.downcase,
+      resource_id: @comment.commentable.id,
+      comment: render_comment
+    )
+  end
+
+  def render_comment
+    CommentsController.renderer.instance_variable_set(
+      :@env, {
+        "HTTP_HOST"=>"localhost:3000", 
+        "HTTPS"=>"off", 
+        "REQUEST_METHOD"=>"GET", 
+        "SCRIPT_NAME"=>"",   
+        "warden" => warden
+      }
+    )
+  
+    CommentsController.render(
+      partial: 'comments/comment',
+      locals: { 
+        comment: @comment,
+        current_user: current_user
+      }
+    )
+  end
 
   def commentable_id
     "#{params[:commentable_type]}_id".downcase.to_sym
